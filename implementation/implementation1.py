9c9e6174f74593df181394d079c4ca4e2eacd6b6
kgh <kgh3489@naver.com>
2022-01-10 23:09:48 +0900

    K번째 약수 구하기

---
+++ implementation/implementation1.py
@@ -0,0 +1,16 @@
+#K번째 약수 구하기
+
+#import sys
+#sys.stdin=open("input.txt", "rt")
+
+n, k=map(int, input().split())
+cnt=0
+
+for i in range(1, n+1):
+    if n%i==0:
+        cnt+=1
+    if cnt==k:
+        print(i)
+        break
+else:
+    print(-1)
