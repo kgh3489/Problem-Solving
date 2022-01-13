6b7c1e5891b5f15616e865a30565e160b80f95a0
kgh <kgh3489@naver.com>
2022-01-13 23:44:38 +0900

    K번째 큰 수 구하기

---
+++ implementation/implementation3.py
@@ -0,0 +1,15 @@
+#K번째 큰 수 구하기
+
+#import sys
+#sys.stdin=open("input.txt", "rt")
+
+n, k=map(int, input().split())
+a=list(map(int, input().split()))
+res=set() #set 자료구조로 변경(중복을 제거함)
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
+            res.add(a[i]+a[j]+a[m])
+res=list(res) # 정렬을 위해 set -> list로 변경
+res.sort(reverse=True) #내림차순으로 정렬
+print(res[k-1])
