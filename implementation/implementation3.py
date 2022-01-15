f9e1ad8b65e56f8b077c77b5dcba5cd894c14d54
kgh <kgh3489@naver.com>
2022-01-15 23:26:30 +0900

    대푯값 구하기

--- implementation/implementation3.py
+++ implementation/implementation3.py
@@ -9,7 +9,7 @@ res=set() #set 자료구조로 변경(중복을 제거함)
 for i in range(n):
     for j in range(i+1, n):
         for m in range(j+1, n):
-            res.add(a[i]+a[j]+a[m])
+            res.add(a[i]+a[j]+a[m]) #append 대신 add
 res=list(res) # 정렬을 위해 set -> list로 변경
 res.sort(reverse=True) #내림차순으로 정렬
 print(res[k-1])
