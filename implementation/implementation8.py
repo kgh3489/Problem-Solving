b9aa2a9de6b4fc4242ff8e641d460b9739fa8246
kgh <kgh3489@naver.com>
2022-01-23 22:58:22 +0900

    뒤집은 소수

--- implementation/implementation8.py
+++ implementation/implementation8.py
@@ -4,11 +4,12 @@
 #sys.stdin=open("input.txt", "r")
 
 n=int(input())
-ch=[0]*(n+1)
-cnt=0
+ch=[0]*(n+1) #소수인지 판별할 리스트 생성
+cnt=0 #소수의 개수를 저장
+
 for i in range(2, n+1):
-    if ch[i]==0:
+    if ch[i]==0: #소수라면
         cnt+=1
-        for j in range(i, n+1, i):
-            ch[j]=1
+        for j in range(i, n+1, i): #나온 소수의 배수들은
+            ch[j]=1 #소수가 아님
 print(cnt)
