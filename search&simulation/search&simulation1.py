2ed84daf953c33a2c62390460f8c371fee01b4b9
kgh <kgh3489@naver.com>
2022-01-28 23:42:10 +0900

    숫자만 추출

--- search&simulation/search&simulation1.py
+++ search&simulation/search&simulation1.py
@@ -4,10 +4,29 @@
 #sys.stdin=open("input.txt", "r")
 
 n=int(input())
+
 for i in range(n):
-    str=input()
-    str=str.upper()
-    if str==str[::-1]:
-        print("#%d YES" %i)
+    s=input()
+    s=s.upper() #대소문자를 구분하지 않기 때문에 모두 대문자로 바꿔줌
+    
+    if s==s[::-1]: #원래 문자와 뒤집은 문자가 같다면
+        print("#%d YES" %(i+1))
     else:
-        print("#%d NO" %i)
+        print("#%d NO" %(i+1))
+
+'''
+
+n=int(input())
+
+for i in range(n):
+    s=input()
+    s=s.upper()
+    size=len(s)
+
+    for j in range(size):
+        if s[j]!=s[-1-j]: #같은 위치의 인덱스를 비교
+            print("#%d NO" %(i+1))
+            break
+        else:
+            print("#%d YES" %(i+1))
+'''
