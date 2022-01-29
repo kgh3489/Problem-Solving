6edb96028e7c3d0581cfcdf4b99ad6d1c72bdd53
kgh <kgh3489@naver.com>
2022-01-29 23:51:06 +0900

    카드 역배치

--- search&simulation/search&simulation2.py
+++ search&simulation/search&simulation2.py
@@ -5,12 +5,14 @@
 
 s=input()
 res=0
+
 for x in s:
-    if x.isdecimal():
+    if x.isdecimal(): #x가 int로 변환 가능하다면
         res=res*10+int(x)
 print(res)
+
 cnt=0
 for i in range(1, res+1):
-    if res%i==0:
+    if res%i==0: #i가 약수라면
         cnt+=1
 print(cnt)
