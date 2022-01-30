e74f7620fd914b898a10005c14e04a522ac793a1
kgh <kgh3489@naver.com>
2022-01-30 23:52:43 +0900

    두 리스트 합치기

--- search&simulation/search&simulation3.py
+++ search&simulation/search&simulation3.py
@@ -3,11 +3,14 @@
 #import sys
 #sys.stdin=open("input.txt", "r")
 
-a=list(range(21))
-for _ in range(10):
+a=list(range(21)) #0-20까지의 리스트 생성
+
+for _ in range(10): #10번 반복
     s, e=map(int, input().split())
     for i in range((e-s+1)//2):
-        a[s+i], a[e-i]=a[e-i], a[s+i]
-a.pop(0)
+        a[s+i], a[e-i]=a[e-i], a[s+i] #해당 위치의 카드 바꿈
+
+a.pop(0) #0번 인덱스 제거
+
 for x in a:
     print(x, end=' ')
