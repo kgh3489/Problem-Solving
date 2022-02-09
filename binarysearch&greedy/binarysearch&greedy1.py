f116df9af1b4d74ee29c833d5f52811bf9f3a65b
kgh <kgh3489@naver.com>
2022-02-09 23:52:33 +0900

    랜선 자르기(결정 알고리즘)

--- binarysearch&greedy/binarysearch&greedy1.py
+++ binarysearch&greedy/binarysearch&greedy1.py
@@ -6,14 +6,15 @@
 n, m=map(int, input().split())
 a=list(map(int, input().split()))
 a.sort()
-lt=0
-rt=n-1
+lt=0 #처음
+rt=n-1 #마지막
+
 while lt<=rt:
     mid=(lt+rt)//2
-    if a[mid]==m:
+    if a[mid]==m: #원하는 값을 찾았다면
         print(mid+1)
         break
-    elif a[mid]>m:
-        rt=mid-1
-    else:
-        lt=mid+1
+    elif a[mid]>m: #원하는 값보다 크다면
+        rt=mid-1 #rt를 이동
+    else: #원하는 값보다 작다면
+        lt=mid+1 #lt를 이동
