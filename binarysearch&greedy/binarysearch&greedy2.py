de12dca413bddc4f9b9865c1fb0a9319ec7fd4c4
kgh <kgh3489@naver.com>
2022-02-10 23:50:19 +0900

    뮤직비디오(결정 알고리즘)

--- binarysearch&greedy/binarysearch&greedy2.py
+++ binarysearch&greedy/binarysearch&greedy2.py
@@ -3,27 +3,32 @@
 #import sys
 #sys.stdin=open("input.txt", "r")
 
-def Count(len):
+def Count(len): #새로 만든 랜선의 개수를 세는 함수
     cnt=0
     for x in Line:
-        cnt+=(x//len)
+        cnt+=x//len
     return cnt
 
 k, n=map(int, input().split())
 Line=[]
 res=0
 largest=0
-for i in range(k):
+
+#가장 긴 랜선을 찾음
+for _ in range(k): 
     tmp=int(input())
     Line.append(tmp)
     largest=max(largest, tmp)
+
 lt=1
 rt=largest
+
 while lt<=rt:
-    mid=(lt+rt)//2
+    mid=(lt+rt)//2 #찾는 랜선의 길이
     if Count(mid)>=n:
         res=mid
         lt=mid+1
     else:
         rt=mid-1
+        
 print(res)
