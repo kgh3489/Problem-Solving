454defa0b814cd7218762499f0ce13d733b21bce
kgh <kgh3489@naver.com>
2022-02-14 23:57:02 +0900

    회의실 배정(그리디)

--- binarysearch&greedy/binarysearch&greedy4.py
+++ binarysearch&greedy/binarysearch&greedy4.py
@@ -7,19 +7,22 @@ def Count(len):
     cnt=1
     ep=Line[0]
     for i in range(1, n):
-        if Line[i]-ep>=len:
+        if Line[i]-ep>=len: #최대 거리보다 멀다면
             cnt+=1
             ep=Line[i]
     return cnt
 
 n, c=map(int, input().split())
-Line=[]
+Line=[] #수직선상의 마구간
+
 for _ in range(n):
     tmp=int(input())
     Line.append(tmp)
 Line.sort()
-lt=1
-rt=Line[n-1]
+
+lt=1 #최소 거리
+rt=Line[n-1] #최대 거리(맨 끝)
+
 while lt<=rt:
     mid=(lt+rt)//2
     if Count(mid)>=c:
@@ -27,7 +30,5 @@ while lt<=rt:
         lt=mid+1
     else:
         rt=mid-1
-
+        
 print(res)
-
-
