2e3471d4fa2a00c4b0faa539d710ba3e4034030a
kgh <kgh3489@naver.com>
2022-02-15 23:49:27 +0900

    씨름 선수(그리디)

--- binarysearch&greedy/binarysearch&greedy5.py
+++ binarysearch&greedy/binarysearch&greedy5.py
@@ -5,14 +5,18 @@
 
 n=int(input())
 meeting=[]
-for i in range(n):
+
+for _ in range(n):
     a, b=map(int, input().split())
-    meeting.append((a, b))
-meeting.sort(key=lambda x : (x[1], x[0]))
+    meeting.append((a, b)) #튜플 형태로 리스트에 추가
+meeting.sort(key=lambda x : (x[1], x[0])) #끝나는 시간을 선순위로 정렬
+
 et=0
 cnt=0
-for x, y in meeting:
-    if x>=et:
-        et=y
+
+for x,y in meeting:
+    if x>=et: #시작 시간이 끝나는 시간 후라면
+        et=y 
         cnt+=1
+
 print(cnt)
