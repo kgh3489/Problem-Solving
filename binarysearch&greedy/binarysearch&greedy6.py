8b638f40330ce27845270f46d5559ccd676fa8b3
kgh <kgh3489@naver.com>
2022-02-19 17:27:51 +0900

    창고 정리(그리디)

--- binarysearch&greedy/binarysearch&greedy6.py
+++ binarysearch&greedy/binarysearch&greedy6.py
@@ -5,13 +5,17 @@
 
 n=int(input())
 body=[]
-for i in range(n):
+
+for _ in range(n):
     a, b=map(int, input().split())
-    body.append((a, b))
-body.sort(reverse=True)
+    body.append((a,b))
+
+body.sort(reverse=True)#키를 내림차순으로 정렬
 largest=0
 cnt=0
+
 for x, y in body:
+    #키가 내림차순을 정렬됐기 때문에 몸무게를 비교하여 저장된 최댓값보다 크다면 카운트
     if y>largest:
         largest=y
         cnt+=1
