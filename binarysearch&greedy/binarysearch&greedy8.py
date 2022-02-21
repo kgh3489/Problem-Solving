afdb1b618cf5df66a0aef4b73ab8bc916760ee37
kgh <kgh3489@naver.com>
2022-02-21 23:40:47 +0900

    증가수열 만들기(그리디)

--- binarysearch&greedy/binarysearch&greedy8.py
+++ binarysearch&greedy/binarysearch&greedy8.py
@@ -3,13 +3,14 @@
 #import sys
 #sys.stdin=open("input.txt", "r")
 
-from collections import deque
+'''
+#기본적인 방법:
 
 n, limit=map(int, input().split())
 p=list(map(int, input().split()))
 p.sort()
-p=deque(p)
 cnt=0
+
 while p:
     if len(p)==1:
         cnt+=1
@@ -18,8 +19,34 @@ while p:
         p.pop()
         cnt+=1
     else:
-        p.popleft()
+        p.pop(0)
         p.pop()
         cnt+=1
-print(cnt)
 
+print(cnt)
+'''
+
+#deque를 활용한 효울적인 방법(연산을 줄임)
+
+from collections import deque
+
+n, limit=map(int, input().split())
+p=list(map(int, input().split()))
+p.sort() #맨 앞과 맨 뒤의 사람을 더하기 위해 정렬
+p=deque(p) #자료구조를 큐로 선언
+cnt=0
+
+while p:
+    if len(p)==1: #한 명만 남아있다면
+        cnt+=1
+        break
+    if p[0]+p[-1]>limit: #제한을 초과하면
+        p.pop() #제일 무거운 사람만 보트에 태움
+        cnt+=1
+    else: #제한보다 적거나 같다면
+        #두 사람을 모두 보트에 태움
+        p.popleft() 
+        p.pop()
+        cnt+=1
+        
+print(cnt)
