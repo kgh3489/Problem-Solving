ed8014d8bac030e54de3b33c2413f497c875ab8e
kgh <kgh3489@naver.com>
2022-02-25 00:03:41 +0900

    역수열(그리디)

--- binarysearch&greedy/binarysearch&greedy9.py
+++ binarysearch&greedy/binarysearch&greedy9.py
@@ -5,27 +5,29 @@
 
 n=int(input())
 a=list(map(int, input().split()))
-lt=0
-rt=n-1
-last=0
-res=""
+lt=0 #왼쪽에서 시작
+rt=n-1 #오른쪽에서 시작
+last=0 #수열의 마지막 수
+res="" 
 tmp=[]
+
 while lt<=rt:
-    if a[lt]>last:
-        tmp.append((a[lt], 'L'))
-    if a[rt]>last:
-        tmp.append((a[rt], 'R'))
-    tmp.sort()
-    if len(tmp)==0:
-        break;
-    else:
-        res=res+tmp[0][1]
-        last=tmp[0][0]
-        if tmp[0][1]=='L':
-            lt=lt+1
+        if a[lt]>last:
+           tmp.append((a[lt], 'L'))
+        if a[rt]>last:
+            tmp.append((a[rt], 'R'))
+        tmp.sort() #append한 숫자들을 정렬
+        
+        if len(tmp)==0: #append 할 숫자가 없다면
+            break
         else:
-            rt=rt-1
-    tmp.clear()
+            res=res+tmp[0][1] #문자열 이어붙임
+            last=tmp[0][0]
+            if tmp[0][1]=='L':
+                lt=lt+1
+            else:
+                rt=rt-1
+            tmp.clear()
 
 print(len(res))
 print(res)
