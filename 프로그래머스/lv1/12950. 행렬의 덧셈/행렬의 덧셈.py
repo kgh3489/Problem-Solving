21286d860dfd2c2b3a49760e7087f06c5a5b3641
KGH <69743800+kgh3489@users.noreply.github.com>
2022-08-07 00:08:26 +0900

    [level 1] Title: 행렬의 덧셈, Time: 29.87 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def solution(arr1, arr2):
-    a = np.array(arr1)
+    #행렬 덧셈을 위해 array로 변환
+    a = np.array(arr1) 
     b = np.array(arr2)
-    return (a + b).tolist()
+    return (a + b).tolist() #다시 list로 변환
