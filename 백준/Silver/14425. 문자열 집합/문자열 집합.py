b6b7708ef26b7f829db66a185baa94ac28dd2ff9
KGH <69743800+kgh3489@users.noreply.github.com>
2022-09-09 20:12:55 +0900

    [Silver III] Title: 문자열 집합, Time: 144 ms, Memory: 36004 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,12 +1,12 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-S = []
+S = set() #set의 탐색  평균 시간복잡도는 O(1)
 cnt = 0
 
 for _ in range(N):
     word = sys.stdin.readline()
-    S.append(word)
+    S.add(word)
 
 for i in range(M):
     check_word = sys.stdin.readline()
