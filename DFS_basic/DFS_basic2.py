ffb012b85b700d7d764a6a2ec12c0b9c10333ae9
kgh3489 <kgh3489@naver.com>
2023-01-21 20:33:49 +0900

    이진트리 순회

---
+++ DFS_basic/DFS_basic2.py
@@ -0,0 +1,15 @@
+# 이진트리 순회
+
+# import sys
+# sys.stdin=open("input.txt", "r")
+
+def DFS(v):
+    if v>7:
+        return
+    else:  
+        print(v, end=' ')
+        DFS(v*2)
+        DFS(v*2+1)
+        
+if __name__=="__main__":
+    DFS(1)
