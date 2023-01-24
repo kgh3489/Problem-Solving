568bf9ebbc8d8e916dfa996559a8a1b2b638e814
kgh3489 <kgh3489@naver.com>
2023-01-24 23:57:41 +0900

    바둑이 승차

---
+++ DFS_basic/DFS_basic5.py
@@ -0,0 +1,35 @@
+# 바둑이 승차
+
+# import sys
+# sys.stdin=open("input.txt", "r")
+
+def DFS(L, sum, tsum): #레벨, 합, 한 번 계산된 합
+    global result
+    
+    if sum+(total-tsum) < result:
+        return
+    if sum > c:
+        return
+    if L == n:
+        if sum > result:
+            result = sum
+    else:
+        DFS(L+1, sum+a[L], tsum+a[L])
+        DFS(L+1, sum, tsum+a[L])
+
+
+if __name__=="__main__":
+    c, n = map(int, input().split())
+    a = [0]*n
+    result = -2147000000
+    
+    for i in range(n):
+        a[i]=int(input())
+        
+    total=sum(a)
+    DFS(0, 0, 0)
+    
+    print(result)
+
+
+    
