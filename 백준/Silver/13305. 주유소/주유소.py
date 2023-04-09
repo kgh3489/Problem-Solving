e5ccc003a0469382b9efca6794169b231b9f5fab
KGH <69743800+kgh3489@users.noreply.github.com>
2023-04-09 22:40:08 +0900

    [Silver III] Title: 주유소, Time: 120 ms, Memory: 46252 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+roads = list(map(int,input().split()))
+costs = list(map(int,input().split()))
+
+# 첫번째 값 더하기
+min_price = roads[0] * costs[0]
+
+# 가장 값이 싼 주유소 지정
+min_cost = costs[0]
+
+for i in range(1, N-1):
+    if min_cost > costs[i]: # 가장 값이 싼 주유소가 현재 주유소 보다 비싸면 바꿔준다.
+        min_cost = costs[i] # 값 싼 주유소로 바꿔주기
+    min_price += min_cost * roads[i]
+
+print(min_price)
+
